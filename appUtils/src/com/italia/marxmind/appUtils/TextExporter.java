f8b1c736b55764bc4994c67e5fb35ad01ee7509a
Mark <Mark@10.158.1.19>
2022-06-30 16:45:28 +0800

    change files

--- appUtils/src/com/italia/marxmind/appUtils/TextExporter.java
+++ appUtils/src/com/italia/marxmind/appUtils/TextExporter.java
@@ -24,127 +24,113 @@ import org.primefaces.util.EscapeUtils;
  */
 public class TextExporter extends DataTableExporter {
 
+	@Override
+	public String getContentType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    private OutputStreamWriter osw;
-    private PrintWriter writer;
-	
-    @Override
-    protected void preExport(FacesContext context, ExportConfiguration exportConfiguration) throws IOException {
+	@Override
+	public String getFileExtension() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-        osw = new OutputStreamWriter(getOutputStream(), exportConfiguration.getEncodingType());
-        writer = new PrintWriter(osw);
+	@Override
+	protected void doExport(FacesContext arg0, DataTable arg1, ExportConfiguration arg2, int arg3) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
-        if (exportConfiguration.getPreProcessor() != null) {
-            exportConfiguration.getPreProcessor().invoke(context.getELContext(), new Object[]{writer});
-        }
-    }
+	@Override
+	protected void exportCells(DataTable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    protected void doExport(FacesContext context, DataTable table, ExportConfiguration exportConfiguration, int index) throws IOException {
 
-        writer.append("" + table.getId() + "\n");
-
-        if (exportConfiguration.isPageOnly()) {
-            exportPageOnly(context, table, writer);
-        } else if (exportConfiguration.isSelectionOnly()) {
-            exportSelectionOnly(context, table, writer);
-        } else {
-            exportAll(context, table, writer);
-        }
-
-        writer.append("" + table.getId() + "");
-
-        table.setRowIndex(-1);
-    }
-
-    @Override
-    protected void postExport(FacesContext context, ExportConfiguration exportConfiguration) throws IOException {
-
-        if (exportConfiguration.getPostProcessor() != null) {
-            exportConfiguration.getPostProcessor().invoke(context.getELContext(), new Object[]{writer});
-        }
-
-        writer.flush();
-        writer.close();
-        writer = null;
-
-        osw.close();
-        osw = null;
-    }
-
-    @Override
-    protected void preRowExport(DataTable table, Object document) {
-        ((PrintWriter) document).append("\t" + table.getVar() + "\n");
-    }
-
-    @Override
-    protected void postRowExport(DataTable table, Object document) {
-        ((PrintWriter) document).append("\t" + table.getVar() + "\n");
-    }
-
-    @Override
-    protected void exportCells(DataTable table, Object document) {
-        PrintWriter writer = (PrintWriter) document;
-        for (UIColumn col : table.getColumns()) {
-            if (col instanceof DynamicColumn) {
-                ((DynamicColumn) col).applyStatelessModel();
-            }
-
-            if (col.isRendered() && col.isExportable()) {
-                String columnTag = getColumnTag(col);
-                addColumnValue(writer, col.getChildren(), columnTag, col);
-            }
-        }
-    }
-
-    protected String getColumnTag(UIColumn column) {
-        String headerText = (column.getExportHeaderValue() != null) ? column.getExportHeaderValue() : column.getHeaderText();
-        UIComponent facet = column.getFacet("header");
-        String columnTag;
-
-        if (headerText != null) {
-            columnTag = headerText.toLowerCase();
-        }
-        else if (facet != null) {
-            columnTag = exportValue(FacesContext.getCurrentInstance(), facet).toLowerCase();
-        }
-        else {
-            throw new FacesException("No suitable xml tag found for " + column);
-        }
-
-        return EscapeUtils.forXmlTag(columnTag);
-    }
-
-    protected void addColumnValue(PrintWriter writer, List<UIComponent> components, String tag, UIColumn column) {
-        FacesContext context = FacesContext.getCurrentInstance();
-
-        writer.append("\t\t" + tag + "");
-
-        if (column.getExportFunction() != null) {
-            writer.append(EscapeUtils.forXml(exportColumnByFunction(context, column)));
-        }
-        else {
-            for (UIComponent component : components) {
-                if (component.isRendered()) {
-                    String value = exportValue(context, component);
-                    if (value != null) {
-                        writer.append(EscapeUtils.forXml(value));
-                    }
-                }
-            }
-        }
-
-        writer.append("" + tag + "\n");
-    }
-
-    @Override
-    public String getContentType() {
-        return "text/plain";
-    }
-
-    @Override
-    public String getFileExtension() {
-        return ".txt";
-    }
+	/*
+	 * private OutputStreamWriter osw; private PrintWriter writer;
+	 * 
+	 * @Override protected void preExport(FacesContext context, ExportConfiguration
+	 * exportConfiguration) throws IOException {
+	 * 
+	 * osw = new OutputStreamWriter(getOutputStream(),
+	 * exportConfiguration.getEncodingType()); writer = new PrintWriter(osw);
+	 * 
+	 * if (exportConfiguration.getPreProcessor() != null) {
+	 * exportConfiguration.getPreProcessor().invoke(context.getELContext(), new
+	 * Object[]{writer}); } }
+	 * 
+	 * @Override protected void doExport(FacesContext context, DataTable table,
+	 * ExportConfiguration exportConfiguration, int index) throws IOException {
+	 * 
+	 * writer.append("" + table.getId() + "\n");
+	 * 
+	 * if (exportConfiguration.isPageOnly()) { exportPageOnly(context, table,
+	 * writer); } else if (exportConfiguration.isSelectionOnly()) {
+	 * exportSelectionOnly(context, table, writer); } else { exportAll(context,
+	 * table, writer); }
+	 * 
+	 * writer.append("" + table.getId() + "");
+	 * 
+	 * table.setRowIndex(-1); }
+	 * 
+	 * @Override protected void postExport(FacesContext context, ExportConfiguration
+	 * exportConfiguration) throws IOException {
+	 * 
+	 * if (exportConfiguration.getPostProcessor() != null) {
+	 * exportConfiguration.getPostProcessor().invoke(context.getELContext(), new
+	 * Object[]{writer}); }
+	 * 
+	 * writer.flush(); writer.close(); writer = null;
+	 * 
+	 * osw.close(); osw = null; }
+	 * 
+	 * @Override protected void preRowExport(DataTable table, Object document) {
+	 * ((PrintWriter) document).append("\t" + table.getVar() + "\n"); }
+	 * 
+	 * @Override protected void postRowExport(DataTable table, Object document) {
+	 * ((PrintWriter) document).append("\t" + table.getVar() + "\n"); }
+	 * 
+	 * @Override protected void exportCells(DataTable table, Object document) {
+	 * PrintWriter writer = (PrintWriter) document; for (UIColumn col :
+	 * table.getColumns()) { if (col instanceof DynamicColumn) { ((DynamicColumn)
+	 * col).applyStatelessModel(); }
+	 * 
+	 * if (col.isRendered() && col.isExportable()) { String columnTag =
+	 * getColumnTag(col); addColumnValue(writer, col.getChildren(), columnTag, col);
+	 * } } }
+	 * 
+	 * protected String getColumnTag(UIColumn column) { String headerText =
+	 * (column.getExportHeaderValue() != null) ? column.getExportHeaderValue() :
+	 * column.getHeaderText(); UIComponent facet = column.getFacet("header"); String
+	 * columnTag;
+	 * 
+	 * if (headerText != null) { columnTag = headerText.toLowerCase(); } else if
+	 * (facet != null) { columnTag = exportValue(FacesContext.getCurrentInstance(),
+	 * facet).toLowerCase(); } else { throw new
+	 * FacesException("No suitable xml tag found for " + column); }
+	 * 
+	 * return EscapeUtils.forXmlTag(columnTag); }
+	 * 
+	 * protected void addColumnValue(PrintWriter writer, List<UIComponent>
+	 * components, String tag, UIColumn column) { FacesContext context =
+	 * FacesContext.getCurrentInstance();
+	 * 
+	 * writer.append("\t\t" + tag + "");
+	 * 
+	 * if (column.getExportFunction() != null) {
+	 * writer.append(EscapeUtils.forXml(exportColumnByFunction(context, column))); }
+	 * else { for (UIComponent component : components) { if (component.isRendered())
+	 * { String value = exportValue(context, component); if (value != null) {
+	 * writer.append(EscapeUtils.forXml(value)); } } } }
+	 * 
+	 * writer.append("" + tag + "\n"); }
+	 * 
+	 * @Override public String getContentType() { return "text/plain"; }
+	 * 
+	 * @Override public String getFileExtension() { return ".txt"; }
+	 */
     
 }
